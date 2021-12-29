ae3137973909820618239a9d3aad3076453bf89e
Yauheni Baradzko <Yauheni.Baradzko@itechart-group.com>
2021-12-29 11:09:32 +0100

    feat: add styles to footer

--- client/components/Layout/components/Footer/Footer.jsx
+++ client/components/Layout/components/Footer/Footer.jsx
@@ -1,6 +1,20 @@
+import { FOOTER_INFO } from '../../../../constants/footer'
+import cl from './Footer.module.scss'
+
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer className={cl.footer}>
+      <div className="container">
+        <div className={cl.contacts}>
+          {FOOTER_INFO.map(({ id, title, text }) => (
+            <div className={cl.item} key={id}>
+              <h3 className={cl.title}>{title}</h3>
+              <p className={cl.text}>{text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
   )
 }
 
